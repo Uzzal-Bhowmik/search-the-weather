2ddfb4b24af3274a16c7e9cf82d02171ad0dea09
Uzzal-Bhowmik <ubcreativity111@gmail.com>
2021-08-31 23:01:25 +0600

    Code formatted

--- js/app.js
+++ js/app.js
@@ -1,43 +1,39 @@
-const searchField = document.getElementById('search-field');
+const searchField = document.getElementById("search-field");
 
-document.getElementById('search-btn').addEventListener('click', function () {
-    if (searchField.value != '') {
+document.getElementById("search-btn").addEventListener("click", function () {
+  if (searchField.value != "") {
+    // calling spinner
+    const spinner = document.getElementById("spinner");
+    spinner.style.display = "block";
+    // calling weather api using city name
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${searchField.value}&units=metric&appid=2b334becae44bdcd12f3c9bf920b53d9`
+    )
+      .then((res) => {
+        hideSpinner();
+        return res.json();
+      })
+      .then((weatherData) => displayWeather(weatherData));
 
-        // calling spinner 
-        const spinner = document.getElementById('spinner');
-        spinner.style.display = 'block';
-        // calling weather api using city name 
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchField.value}&units=metric&appid=2b334becae44bdcd12f3c9bf920b53d9`)
-            .then(res => {
-                hideSpinner();
-                return res.json()
-            })
-            .then(weatherData => displayWeather(weatherData))
-
-        // clearing search field 
-        searchField.value = ''
-    }
-    else {
-        displayError(1);
-    }
+    // clearing search field
+    searchField.value = "";
+  } else {
+    displayError(1);
+  }
 });
 
-
 const displayWeather = (weatherInfo) => {
+  if (weatherInfo.cod == "404") {
+    const weatherInfoCon = document.getElementById("weather-info-container");
+    weatherInfoCon.textContent = "";
+    displayError(1);
+  } else {
+    const weatherInfoCon = document.getElementById("weather-info-container");
+    displayError(0);
+    weatherInfoCon.textContent = "";
 
-    if (weatherInfo.cod == '404') {
-        const weatherInfoCon = document.getElementById('weather-info-container');
-        weatherInfoCon.textContent = '';
-        displayError(1);
-    }
-    else {
-
-        const weatherInfoCon = document.getElementById('weather-info-container');
-        displayError(0)
-        weatherInfoCon.textContent = '';
-
-        const weather = document.createElement('div')
-        weather.innerHTML = `
+    const weather = document.createElement("div");
+    weather.innerHTML = `
         <img src="https://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}@2x.png" alt="">
         <h1 id='place-name'>${weatherInfo.name}</h1>
         <pre>Country: ${weatherInfo.sys.country}</pre>
@@ -49,41 +45,42 @@ const displayWeather = (weatherInfo) => {
         </div>
     `;
 
-        weatherInfoCon.appendChild(weather);
-    }
+    weatherInfoCon.appendChild(weather);
+  }
 };
 
-// Loading time information when time button is clicked 
+// Loading time information when time button is clicked
 function loadTime() {
-    const timePlace = document.getElementById('place-name');
-    fetch(`https://api.ipgeolocation.io/timezone?apiKey=bb457660a4c742e4878a63e0822723ec&location=${timePlace.innerText}`)
-        .then(res => res.json())
-        .then(data => displayTime(data))
+  const timePlace = document.getElementById("place-name");
+  fetch(
+    `https://api.ipgeolocation.io/timezone?apiKey=bb457660a4c742e4878a63e0822723ec&location=${timePlace.innerText}`
+  )
+    .then((res) => res.json())
+    .then((data) => displayTime(data));
 }
 
 const displayTime = (time) => {
-    const container = document.getElementById('time-con');
-    container.textContent = '';
-    const timeToAdd = document.createElement('p');
-    timeToAdd.innerHTML = `
+  const container = document.getElementById("time-con");
+  container.textContent = "";
+  const timeToAdd = document.createElement("p");
+  timeToAdd.innerHTML = `
     <br>
     <span class='fw-bold bg-white text-dark p-2 mt-4 border border-info rounded'>at ${time.time_12} (Local Time)</span>
     `;
-    container.appendChild(timeToAdd);
-}
-
-// error showing function 
-function displayError(id) {
-    const errorCon = document.getElementById('error-con');
-    errorCon.textContent = '';
-    if (id == 1) {
-        errorCon.innerHTML = `
-    <h1 class="text-white">Nothing was found :(</h1>
-    `;
-    }
+  container.appendChild(timeToAdd);
 };
 
+// error showing function
+function displayError(id) {
+  const errorCon = document.getElementById("error-con");
+  errorCon.textContent = "";
+  if (id == 1) {
+    errorCon.innerHTML = `
+    <h1 class="text-white">Nothing was found :(</h1>
+    `;
+  }
+}
+
 function hideSpinner() {
-    document.getElementById('spinner')
-        .style.display = 'none';
+  document.getElementById("spinner").style.display = "none";
 }
